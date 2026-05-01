f64878d6246c876253f4df062e2c721b2389db50
Hasanboy1295 <jabborovhasan853@gmail.com>
2026-05-01 14:57:57 +0900

    fix: port api

--- src/server.ts
+++ src/server.ts
@@ -45,7 +45,7 @@ mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then(() => {
     console.log("MongoDB connection succeed");
-    const PORT = process.env.PORT ?? 3003;
+    const PORT = process.env.PORT ?? 3025;
     server.listen(PORT, function () {
       console.info(`The server is running successfully on port: ${PORT}`);
       console.info(`Admin project on http://localhost:${PORT}/admin \n`);
